610612b11e12e594f2f8e27b804b83d58db75551
Joana Martins <joana_smar@hotmail.com>
2016-11-11 15:55:45 +0000

    Add files via upload

--- netMHCIIpan_script.sh
+++ netMHCIIpan_script.sh
@@ -2,9 +2,9 @@
 
 tput setaf 1
 echo "Hello!"
-echo """Please make sure you have the file base.txt in this directory, and the input file (example idh1.fasta)
-If you lost base.txt file you can recover it from this script ( at the end of netMHCIIpan_script.sh)
-If your input file has a name differnt from idh1.fasta you have to replace it in base.txt"""
+echo """Please make sure you have the file base.txt in this directory, and the input file (example consensus.fasta)
+If you lost base.txt file you can recover it from this script ( at the end of netMHCpan_script.sh)
+If your input file has a name differnt from consensus.fasta you have to replace it in base.txt"""
 echo "Starting..."
 echo "Getting human HLAs for netMHCpan"
 
@@ -33,19 +33,13 @@ echo "My job here is Done"
 
 <<COMMENT1
 base.txt -> copy the following:
-../netMHCIIpan -a old -f 9peptide_idh1.fasta -length 9 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_9.txt
-../netMHCIIpan -a old -f 10peptide_idh1.fasta -length 10 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_10.txt
-../netMHCIIpan -a old -f 11peptide_idh1.fasta -length 11 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_11.txt
-../netMHCIIpan -a old -f 12peptide_idh1.fasta -length 12 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_12.txt
-../netMHCIIpan -a old -f 13peptide_idh1.fasta -length 13 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_13.txt
-../netMHCIIpan -a old -f 14peptide_idh1.fasta -length 14 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_14.txt
-../netMHCIIpan -a old -f 15peptide_idh1.fasta -length 15 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_15.txt
-../netMHCIIpan -a old -f 16peptide_idh1.fasta -length 16 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_16.txt
-../netMHCIIpan -a old -f 17peptide_idh1.fasta -length 17 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_17.txt
-../netMHCIIpan -a old -f 18peptide_idh1.fasta -length 18 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_18.txt
-../netMHCIIpan -a old -f 19peptide_idh1.fasta -length 19 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_19.txt
-../netMHCIIpan -a old -f 20peptide_idh1.fasta -length 20 -affS 50.0 -affW 500.0 -rankS 0.50 -rankW 2.0 > ./out/old_20.txt
-
+../../netMHCpan -a old -f consensus.fasta -l 8 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_8.txt
+../../netMHCpan -a old -f consensus.fasta -l 9 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_9.txt
+../../netMHCpan -a old -f consensus.fasta -l 10 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_10.txt
+../../netMHCpan -a old -f consensus.fasta -l 11 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_11.txt
+../../netMHCpan -a old -f consensus.fasta -l 12 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_12.txt
+../../netMHCpan -a old -f consensus.fasta -l 13 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_13.txt
+../../netMHCpan -a old -f consensus.fasta -l 14 -s -th 50.0 -lt 500.0 -rth 0.50 -rlt 2.0 -ic50 > ./out/old_14.txt
 COMMENT1
 
 tput sgr0
